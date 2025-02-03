9a6dc03b0d6287aaaf2b1e630979dbd3f1920dbb
Raian Fernandes dos Santos <raianfs@gmail.com>
2025-02-03 10:58:20 -0300

    Fix folder name

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 systemctl stop oriondns
 
 cd backend
-go build -o /usr/bin/oriondns ./cmd/oriondns/*.go
+go build -o /usr/bin/oriondns ./cmd/dnsserver/*.go
 cp ./config/production.yaml /etc/oriondns.yaml
 
 systemctl restart oriondns
