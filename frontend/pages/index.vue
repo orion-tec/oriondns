c86e0a6e18763dbd882eade8c49aef73826515bb
itsraian <104225187+itsraian@users.noreply.github.com>
2025-02-20 13:55:51 -0300

    Replace chartjs with echarts (#38)
    * Replace chartjs with echart
    
    * Replace ChartJS with ECharts

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,93 +1,25 @@
 <script setup lang="ts">
-import { Line } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  Colors,
-  PointElement,
-  LineElement,
-  scales,
-} from "chart.js";
-import type { callback } from "chart.js/helpers";
-
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  Colors,
-  PointElement,
-  LineElement,
-);
-
-const now = new Date();
-const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-const yesterday = new Date(today);
-yesterday.setDate(yesterday.getDate() - 1);
-
-const tomorrow = new Date(today);
-tomorrow.setDate(tomorrow.getDate() + 1);
-
-const renderDate = (date: Date) => {
-  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-};
-
-const labels = [yesterday, today, tomorrow];
-
-const chartData = {
-  labels,
-  datasets: [
-    {
-      label: "Random data",
-      data: [40, 20, 50],
-      tension: 0.5,
-      fill: false,
-      borderColor: "red",
-    },
-  ],
-};
-
-const chartOptions = {
-  responsive: true,
-  scales: {
-    x: {
-      display: true,
-      title: {
-        display: true,
-        text: "Date",
+const option = ref({
+  dataset: {
+    dimensions: ["Product", "2015", "2016", "2017"],
+    source: [
+      {
+        Product: "Matcha Latte",
+        2015: 54,
+        2016: 42,
+        2017: 23,
       },
-      ticks: {
-        callback: (value: number | string) => {
-          return renderDate(labels[value]);
-        },
-      },
-    },
+    ],
   },
-  plugins: {
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
-    legend: {
-      labels: {
-        color: "white",
-      },
-      title: {
-        text: "Legend",
-      },
-    },
-  },
-};
+  xAxis: { type: "category" },
+  yAxis: {},
+  series: [{ type: "bar" }],
+});
 </script>
 
 <template>
   <div class="chart-container">
-    <Line id="my-chart-id" :options="chartOptions" :data="chartData" />
+    <VChart :option="option" />
   </div>
 </template>
 
