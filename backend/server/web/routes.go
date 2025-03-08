7489e8e31e4e79ef85240fa193afc8fa05981428
itsraian <104225187+itsraian@users.noreply.github.com>
2025-03-08 17:04:37 -0300

    Extracter DTOS and Handlers (#61)

---
+++ backend/server/web/routes.go
@@ -0,0 +1,14 @@
+package web
+
+import "net/http"
+
+func withCors(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func (h *HTTP) setupRoutes() {
+	http.HandleFunc("POST /api/v1/dashboard/most-used-domains", withCors(h.getMostUsedDomainsDashboard))
+}
