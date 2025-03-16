dad3bed2fdecb0cc0e6c03e305dc0ef8fe10eaba
itsraian <104225187+itsraian@users.noreply.github.com>
2025-03-16 19:12:56 -0300

    Fix dashboard timezone (#82)
    * Fix dashboard timezone
    
    * Remove prints

--- frontend/utils/date.ts
+++ frontend/utils/date.ts
@@ -7,12 +7,22 @@ export type TimeRange =
   | "Yesterday";
 
 export const dateAtUTC = (t: Date) => {
-  return new Date(t.getTime() + t.getTimezoneOffset() * 60000);
+  //return new Date(t.getTime() + t.getTimezoneOffset() * 60000);
+  return new Date(
+    Date.UTC(
+      t.getUTCFullYear(),
+      t.getUTCMonth(),
+      t.getUTCDate(),
+      t.getUTCHours(),
+      t.getUTCMinutes(),
+      t.getUTCSeconds(),
+    ),
+  );
 };
 
 export const dateAtCurrentTZ = (t: string) => {
   const newTime = new Date(t);
-  return new Date(newTime.getTime() - newTime.getTimezoneOffset() * 60000);
+  return newTime;
 };
 
 export const formatDate = (date: Date) => {
@@ -23,26 +33,27 @@ export const formatDate = (date: Date) => {
 };
 
 export const getDateFromRange = (range: TimeRange): { from: Date; to: Date } => {
-  const midnight = new Date(new Date().setHours(0, 0, 0, 0));
+  const midnight = new Date();
+  midnight.setHours(0, 0, 0, 0);
   const now = new Date();
 
   let t = { from: new Date(), to: new Date() };
 
   switch (range) {
     case "Last month":
-      t = { from: new Date(new Date().setMonth(midnight.getMonth() - 1)), to: now };
+      t = { from: new Date(midnight.getTime() - 30 * 24 * 60 * 60 * 1000), to: now };
       break;
     case "Last week":
-      t = { from: new Date(new Date().setDate(midnight.getDate() - 7)), to: now };
+      t = { from: new Date(midnight.getTime() - 7 * 24 * 60 * 60 * 1000), to: now };
       break;
     case "Last 2 weeks":
-      t = { from: new Date(new Date().setDate(midnight.getDate() - 14)), to: now };
+      t = { from: new Date(midnight.getTime() - 14 * 24 * 60 * 60 * 1000), to: now };
       break;
     case "Last 3 days":
-      t = { from: new Date(new Date().setDate(midnight.getDate() - 3)), to: now };
+      t = { from: new Date(midnight.getTime() - 3 * 24 * 60 * 60 * 1000), to: now };
       break;
     case "Yesterday":
-      t = { from: new Date(new Date().setDate(midnight.getDate() - 1)), to: midnight };
+      t = { from: new Date(midnight.getTime() - 24 * 60 * 60 * 1000), to: midnight };
       break;
     case "Today":
     default:
